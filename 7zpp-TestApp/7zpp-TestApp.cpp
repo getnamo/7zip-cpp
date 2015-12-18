038154c77708bba04439afc6c6b2db72d083eb0c
Keith J. Jones <keith@keithjjones.com>
2015-12-18 16:54:15 -0500

    Added a zip lister test.

--- 7zpp-TestApp/7zpp-TestApp.cpp
+++ 7zpp-TestApp/7zpp-TestApp.cpp
@@ -2,12 +2,43 @@
 //
 
 #include "stdafx.h"
+#include <iostream>
 #include "../7zpp/7zpp.h"
+
+class ListCallBackOutput : SevenZip::ListCallback
+{
+	virtual void OnFileFound(WCHAR* path, int size)
+	{
+		std::wcout
+			<< path
+			<< L" "
+			<< size
+			<< std::endl;
+	}
+};
+
 
 int main()
 {
 
+	SevenZip::SevenZipLibrary lib;
+	lib.Load();
 
-    return 0;
+	SevenZip::TString myArchive(L"ReadMe.zip");
+
+	//SevenZip::TString myDest(L"temp");
+	//SevenZip::SevenZipExtractor extractor(lib, myArchive);
+	//extractor.SetCompressionFormat(SevenZip::CompressionFormat::Zip);
+	//extractor.ExtractArchive(myDest, nullptr);
+
+	ListCallBackOutput myListCallBack;
+
+	SevenZip::TString myDir(L"");
+
+	SevenZip::SevenZipLister lister(lib, myArchive);
+	lister.SetCompressionFormat(SevenZip::CompressionFormat::Zip);
+	lister.ListArchive(myDir, (SevenZip::ListCallback *)&myListCallBack);
+
+	return 0;
 }
 
