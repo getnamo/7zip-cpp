7e1bc4351cc017768f3bc61ce89a4c76ce00bf80
Keith J. Jones <keith@keithjjones.com>
2015-12-21 04:47:06 -0500

    Cleaned test application.

--- 7zpp-TestApp/7zpp-TestApp.cpp
+++ 7zpp-TestApp/7zpp-TestApp.cpp
@@ -8,36 +8,6 @@
 #include "../7zpp/7zpp.h"
 
 //
-// 7z.dll directly
-//
-//
-//#include "../7z/CPP/Common/MyWindows.h"
-//#include "../7z/CPP/Common/Defs.h"
-//#include "../7z/CPP/Common/MyInitGuid.h"
-//#include "../7z/CPP/Common/IntToString.h"
-//#include "../7z/CPP/Common/StringConvert.h"
-//#include "../7z/CPP/Windows/DLL.h"
-//#include "../7z/CPP/Windows/FileDir.h"
-//#include "../7z/CPP/Windows/FileFind.h"
-//#include "../7z/CPP/Windows/FileName.h"
-//#include "../7z/CPP/Windows/NtCheck.h"
-//#include "../7z/CPP/Windows/PropVariant.h"
-//#include "../7z/CPP/Windows/PropVariantConv.h"
-//#include "../7z/CPP/7zip/Common/FileStreams.h"
-//#include "../7z/CPP/7zip/Archive/IArchive.h"
-//#include "../7z/CPP/7zip/IPassword.h"
-//#include "../7z/C/7zVersion.h"
-//
-//DEFINE_GUID(CLSID_CFormat7z,
-//	0x23170F69, 0x40C1, 0x278A, 0x10, 0x00, 0x00, 0x01, 0x10, 0x07, 0x00, 0x00);
-//DEFINE_GUID(CLSID_CFormatXz,
-//	0x23170F69, 0x40C1, 0x278A, 0x10, 0x00, 0x00, 0x01, 0x10, 0x0C, 0x00, 0x00);
-//
-//#define CLSID_Format CLSID_CFormat7z
-//// #define CLSID_Format CLSID_CFormatXz
-
-
-//
 // For wrappers...
 //
 class ListCallBackOutput : SevenZip::ListCallback
